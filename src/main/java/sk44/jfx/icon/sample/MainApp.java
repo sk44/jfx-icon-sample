262adeea8d13e57168175a3f25da4c9398f7530b
sk44 <zombie.squad.69.cbt@gmail.com>
2016-12-11 12:05:24 +0900

    Mac 用インストーラー背景画像とウィンドウタイトルアイコンを追加

--- src/main/java/sk44/jfx/icon/sample/MainApp.java
+++ src/main/java/sk44/jfx/icon/sample/MainApp.java
@@ -5,6 +5,7 @@ import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -16,6 +17,7 @@ public class MainApp extends Application {
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
 
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/icon.png")));
         stage.setTitle("JavaFX Icon Sample");
         stage.setScene(scene);
         stage.show();
